6af7cae7216ae95435b6ef25102110b7f434ab04
theantidoak <tcdoak@gmail.com>
2022-04-18 16:17:24 -0400

    Use overflow-x: auto to handle long equations

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const inputDiv = document.querySelector('#input-div');
 const oldInput = document.querySelector('#old-inputs');
 const buttons = document.querySelectorAll('button');
 let previousValue = '';
-const symbols = ['√', '^', '÷', '×', '−', '+', '=', ')', '('];
-const displaySymbols = ['√', '^', '÷', '×', '−', '+', ')', '('];
+const symbols = ['√', '^', '÷', '×', '−', '+', '(', ')', '.'];
 
 buttons.forEach(button => button.addEventListener('click', displayNumbers));
 buttons.forEach(button => button.addEventListener('click', operate));
@@ -11,26 +10,38 @@ buttons.forEach(button => button.addEventListener('click', operate));
 function displayNumbers() {
   let currentValue = this.value;
   const textNode = document.createTextNode(`${currentValue}`);
-  const textP = document.createElement('span');
-  textP.appendChild(textNode);
+
+  // if (previousValue == '√') {
+  //   if (currentValue < 10) return;
+  //   else {
+  //     const textNode = document.createTextNode(`${inputDiv.textContent}` + ' ' + `${currentValue}` + ' ');
+  //     oldInput.append(textNode);
+  //   }
+  // }
 
   if (currentValue == '=') {
     if (currentValue == previousValue) return;
-    const textNode = document.createTextNode(`${currentValue}` + ' ' + `${inputDiv.textContent}` + ' ');
-    oldInput.insertBefore(textNode, oldInput.firstChild);
+    const textNode = document.createTextNode(`${inputDiv.textContent}` + ' ' + `${currentValue}` + ' ');
+    oldInput.append(textNode);
     while (inputDiv.firstChild) {
       inputDiv.removeChild(inputDiv.lastChild);
     }
-  } else if (inputDiv.textContent.includes(')') || inputDiv.textContent.includes('(')) {
-    if (currentValue < 10 || displaySymbols.some(symbol => symbol == currentValue)) {
-      inputDiv.appendChild(textP);
+  } else if (inputDiv.textContent.includes('(') || inputDiv.textContent.includes(')')) {
+    if (currentValue < 10 || symbols.some(symbol => symbol == currentValue)) {
+      inputDiv.appendChild(textNode);
     }
-  } else if (currentValue < 10 || currentValue == ')' || currentValue == '(') {
-    inputDiv.appendChild(textP);
+  } else if (currentValue < 10 || currentValue == '(' || currentValue == ')' || currentValue == '.' || currentValue == '^') {
+    inputDiv.appendChild(textNode);
+  } else if (currentValue == '√') {
+    const textNode = document.createTextNode(`${currentValue}`+`${inputDiv.textContent}` + ' ');
+    oldInput.append(textNode);
+    while (inputDiv.firstChild) {
+      inputDiv.removeChild(inputDiv.lastChild);
+    }
   } else if (symbols.some(symbol => symbol == currentValue)) {
     if (symbols.includes(previousValue)) return;
-    const textNode = document.createTextNode(`${currentValue}` + ' ' + `${inputDiv.textContent}` + ' ');
-    oldInput.insertBefore(textNode, oldInput.firstChild);
+    const textNode = document.createTextNode(`${inputDiv.textContent}` + ' ' + `${currentValue}` + ' ');
+    oldInput.append(textNode);
     while (inputDiv.firstChild) {
       inputDiv.removeChild(inputDiv.lastChild);
     }
