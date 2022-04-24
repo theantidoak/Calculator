8938700cb313148b1404caecf2988e83b4d54c75
theantidoak <tcdoak@gmail.com>
2022-04-24 15:17:30 -0400

    Clean code and add fibonacci button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const inputDiv = document.querySelector("#input-div");
+const newInput = document.querySelector("#input-div");
 const oldInput = document.querySelector("#old-inputs");
 const buttons = document.querySelectorAll("button");
 const operators = ["÷", "×", "−", "+"];
@@ -35,7 +35,7 @@ const stringToNumber = {
 };
 
 let numArray = [];
-let previousNumString;
+let answer;
 let operator;
 let equalFlag = false;
 let operatorFlag = false;
@@ -44,241 +44,217 @@ let currentValue;
 function createFirstNum() {
   currentValue = this.value;
 
-  // Clear Button
   if (currentValue == "clear") {
     start();
   }
 
-  // Delete Button
-  if (currentValue == "delete") {
-    numArray.pop();
-    inputDiv.textContent = numArray.join("");
-  }
-
-  if (currentValue == "^" || (operator == "^" && currentValue == "=")) {
+  if (currentValue == "^" && !operatorFlag 
+    || (operator == "^" && currentValue == "=")) {
     square();
   }
 
-  // Square Root
-  if (currentValue == "√") {
+  if (currentValue == "√" && !operatorFlag) {
     findSquareRoot();
   }
-
-  // Making it negative
-  if (currentValue == "-") {
-    makeNegative();
+  
+  if (currentValue == "delete") {
+    numArray.pop();
+    newInput.textContent = numArray.join("");
   }
 
-  // Equals
+  if (currentValue < 10 || currentValue == '.') {
+    makeNumber();
+  }
+
+  if (currentValue == "-") {
+    toggleNegative();
+  }
+
   if (!equalFlag && currentValue == "=" && operator != "^" && !operatorFlag) {
     equate();
   }
 
-  // Creating the inputDiv array
-  if (currentValue < 10) {
-    makeNumber();
-  }
-
-  // Operators
   if (operators.some((op) => op == currentValue)) {
     operate();
   }
 }
 
 function start() {
-  inputDiv.textContent = "";
+  newInput.textContent = "";
   oldInput.textContent = "";
   operator = undefined;
   numArray = [];
-  previousNumString = undefined;
+  answer = undefined;
   equalFlag = false;
   operatorFlag = false;
 }
 
-function equate() {
-  num = parseFloat(numArray.join(""));
-  previousNumString = parseFloat(
-    stringToNumber[operator](previousNumString, num)
-  );
-  previousInput = document.createTextNode(
-    `${num}` + " " + `${currentValue}` + " "
-  );
-  oldInput.appendChild(previousInput);
-  if (isNaN(previousNumString) || previousNumString == Infinity) {
-    start();
-    inputDiv.textContent = "Error";
-    previousNumString == undefined;
-  } else {
-    inputDiv.textContent = previousNumString;
-  }
-  numArray = [0];
-  equalFlag = true;
-}
-
+// Clear Button
 function clearAll() {
   if (currentValue == "clear") {
     start();
   }
 }
 
-function operate() {
-  // Equation
-  if (previousNumString == undefined) {
-    previousNumString = parseFloat(numArray.join(""));
-    num = previousNumString;
-  } else if (!equalFlag) {
-    if (operatorFlag) {
-      equalFlag = true;
-    } else {
-      num = parseFloat(numArray.join(""));
-      previousNumString = parseFloat(
-        stringToNumber[operator](previousNumString, num)
-      );
-    }
-  }
-
-  // oldInput Text
+// Square the number
+function square() {
   if (equalFlag) {
-    if (num == 1) {
-      previousInput = document.createTextNode(`${currentValue}` + " ");
-      oldInput.appendChild(previousInput);
-    } else {
-      previousInput = document.createTextNode(
-        `${previousNumString}` + " " + `${currentValue}` + " "
-      );
-      oldInput.appendChild(previousInput);
-    }
+    oldInputTextNode = document.createTextNode(
+      `${answer}` + " " + `${currentValue}` + " ");
+    oldInput.appendChild(oldInputTextNode);
+    newInput.textContent = answer + currentValue;
     equalFlag = false;
-  } else {
-    previousInput = document.createTextNode(
-      `${num}` + " " + `${currentValue}` + " "
+    numArray = [answer, currentValue];
+    operator = "^";
+  } else if (!equalFlag && operator == "^") {
+    num = numArray.splice((numArray.indexOf("^") + 1));
+    num = parseFloat(num.join(""));
+    answer = parseFloat(stringToNumber[operator](answer, num));
+    oldInputTextNode = document.createTextNode(
+      `${num}` + " " + "=" + " " + `${answer}` + " " + "=" + " "
     );
-    oldInput.appendChild(previousInput);
-  }
-
-  // inputDiv Text
-  if (isNaN(previousNumString)) {
-    start();
-    inputDiv.textContent = "Error";
-  } else if (previousNumString == Infinity) {
-    start();
-    previousNumString = 0;
-    inputDiv.textContent = previousNumString;
+    oldInput.appendChild(oldInputTextNode);
+    newInput.textContent = answer;
+    equalFlag = true;
   } else {
-    inputDiv.textContent = previousNumString;
+    answer = parseFloat(newInput.textContent);
+    oldInputTextNode = document.createTextNode(
+      `${answer}` + " " + `${currentValue}` + " "
+    );
+    oldInput.appendChild(oldInputTextNode);
+    operator = currentValue;
+    numArray.push(currentValue);
+    newInput.textContent = numArray.join("");
+    }
   }
 
-  numArray = [];
-  operator = currentValue;
-  operatorFlag = true;
+
+// Square Root
+function findSquareRoot() {
+  if (!equalFlag) {
+    answer = parseFloat(numArray.join(""));
+    oldInputTextNode = answer;
+    num = 1;
+    answer = parseFloat(stringToNumber[currentValue](answer, num));
+    oldInputTextNode = document.createTextNode(
+      `${currentValue}` + " " + `${oldInputTextNode}` + " " + "=" + " " + 
+      `${answer}` + " " + "=" + " ");
+    oldInput.appendChild(oldInputTextNode);
+    newInput.textContent = answer;
+    equalFlag = true;
+  } else if (equalFlag) {
+    num = 1;
+    oldInputTextNode = answer;
+    answer = parseFloat(stringToNumber[currentValue](answer, num));
+    oldInputTextNode = document.createTextNode(
+      `${currentValue}` + `${oldInputTextNode}` + " " + "=" + " " +
+      `${answer}` + " " + "=" + " ");
+    oldInput.appendChild(oldInputTextNode);
+    newInput.textContent = answer;
+  }
 }
 
+// Create a Number and/or decimal
 function makeNumber() {
   if (equalFlag) {
     start();
     numArray.push(currentValue);
-    inputDiv.textContent = numArray.join("");
+    newInput.textContent = numArray.join("");
   } else {
     numArray.push(currentValue);
-    inputDiv.textContent = numArray.join("");
+    newInput.textContent = numArray.join("");
   }
   operatorFlag = false;
 }
 
-function makeNegative() {
+// Toggle Negative sign
+function toggleNegative() {
   if (!equalFlag) {
     if (numArray[0] == "-") {
       numArray.shift();
     } else {
       numArray.unshift(currentValue);
     }
-    inputDiv.textContent = numArray.join("");
+    newInput.textContent = numArray.join("");
   } else if (equalFlag) {
-    numArray = [previousNumString];
-    if (numArray[0] == "-" || previousNumString < 0) {
+    numArray = [answer];
+    if (numArray[0] == "-" || answer < 0) {
       numArray = numArray * -1;
-      previousNumString = previousNumString * -1;
-      inputDiv.textContent = numArray;
+      answer = answer * -1;
+      newInput.textContent = numArray;
     } else {
       numArray.unshift(currentValue);
-      previousNumString = -previousNumString;
-      inputDiv.textContent = numArray.join("");
+      answer = -answer;
+      newInput.textContent = numArray.join("");
     }
   }
 }
 
-function square() {
-  if (equalFlag && currentValue == "^") {
-    previousInput = document.createTextNode(
-      `${previousNumString}` + " " + `${currentValue}` + " "
-    );
-    oldInput.appendChild(previousInput);
-    inputDiv.textContent = previousNumString + currentValue;
-    equalFlag = false;
-    numArray = [previousNumString, currentValue];
-    operator = "^";
-  } else if (currentValue == "^") {
-    previousNumString = parseFloat(inputDiv.textContent);
-    previousInput = document.createTextNode(
-      `${previousNumString}` + " " + `${currentValue}` + " "
-    );
-    oldInput.appendChild(previousInput);
-    operator = currentValue;
-    numArray.push(currentValue);
-    inputDiv.textContent = numArray.join("");
-  } else if (operator == "^" && currentValue == "=" && !equalFlag) {
-    let index = numArray.indexOf("^") + 1;
-    num = numArray.splice(index);
-    num = parseFloat(num.join(""));
-    previousNumString = parseFloat(
-      stringToNumber[operator](previousNumString, num)
-    );
-    previousInput = document.createTextNode(
-      `${num}` + " " + "=" + " " + `${previousNumString}` + " " + "=" + " "
-    );
-    oldInput.appendChild(previousInput);
-    inputDiv.textContent = previousNumString;
-    equalFlag = true;
+// Use equal sign
+function equate() {
+  num = parseFloat(numArray.join(""));
+  answer = parseFloat(stringToNumber[operator](answer, num));
+  oldInputTextNode = document.createTextNode(
+    `${num}` + " " + `${currentValue}` + " "
+  );
+  oldInput.appendChild(oldInputTextNode);
+  if (isNaN(answer) || answer == Infinity) {
+    start();
+    newInput.textContent = "Error";
+    answer == undefined;
+  } else {
+    newInput.textContent = answer;
   }
+  numArray = [0];
+  equalFlag = true;
+  operatorFlag = false;
 }
 
-function findSquareRoot() {
-  if (!equalFlag) {
-    previousNumString = parseFloat(numArray.join(""));
-    previousInput = previousNumString;
-    num = 1;
-    previousNumString = parseFloat(
-      stringToNumber[currentValue](previousNumString, num)
-    );
-    previousInput = document.createTextNode(
-      `${currentValue}` +
-        " " +
-        `${previousInput}` +
-        " " +
-        "=" +
-        " " +
-        `${previousNumString}` +
-        " "
-    );
-    oldInput.appendChild(previousInput);
-    inputDiv.textContent = previousNumString;
-    equalFlag = true;
-  } else if (equalFlag) {
-    num = 1;
-    previousInput = previousNumString;
-    previousNumString = parseFloat(
-      stringToNumber[currentValue](previousNumString, num)
-    );
-    previousInput = document.createTextNode(
-      `${currentValue}` +
-        `${previousInput}` +
-        " " +
-        "=" +
-        " " +
-        `${previousNumString}` +
-        " "
-    );
-    oldInput.appendChild(previousInput);
-    inputDiv.textContent = previousNumString;
+// Use the operators
+function operate() {
+  // Equation
+  if (answer == undefined) {
+    answer = parseFloat(numArray.join(""));
+    num = answer;
+  } else if (!equalFlag) {
+    if (operatorFlag) {
+      equalFlag = true;
+    } else {
+      num = parseFloat(numArray.join(""));
+      answer = parseFloat(stringToNumber[operator](answer, num));
+    }
   }
+
+  // oldInput Text
+  if (equalFlag) {
+    if (num == 1) {
+      oldInputTextNode = document.createTextNode(`${currentValue}` + " ");
+      oldInput.appendChild(oldInputTextNode);
+    } else {
+      oldInputTextNode = document.createTextNode(
+        `${answer}` + " " + `${currentValue}` + " ");
+      oldInput.appendChild(oldInputTextNode);
+    }
+    equalFlag = false;
+  } else {
+    oldInputTextNode = document.createTextNode(
+      `${num}` + " " + `${currentValue}` + " ");
+    oldInput.appendChild(oldInputTextNode);
+  }
+
+  // newInput Text
+  if (isNaN(answer)) {
+    start();
+    newInput.textContent = "Error";
+  } else if (answer == Infinity) {
+    start();
+    answer = 0;
+    newInput.textContent = answer;
+  } else {
+    newInput.textContent = answer;
+  }
+
+  numArray = [];
+  operator = currentValue;
+  operatorFlag = true;
 }
